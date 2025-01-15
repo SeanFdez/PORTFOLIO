1245584385a69d257bacab88d29f49b33144ef94
SeanFdez <danielfdez82@gmail.com>
2025-01-15 19:19:29 +0100

    comentarios
    añadidos comentarios del texto

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
+//Localizamos en el documento los elementos del menu, el header y el main.
 const header    =document.querySelector(`.Header`)
 const boton     =header.querySelector(`.Header-menu`)
 const main      =document.querySelector(`.Main`)
 const menu      =header.querySelector(`.Header-ul`)
-console.log(boton)
 
+//Al pùlsar el boton, hacemos toggle de la clase Activo en los tres elementos afectados.
 boton.addEventListener(`click`, ()=>{
     boton.classList.toggle(`Activo`)
     main.classList.toggle(`Activo`)
